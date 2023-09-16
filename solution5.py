21fc5ead13f02ba11b46191f1b30d3b256c61258
HernandezJC <jchernandez1021@gmail.com>
2023-09-16 12:25:59 -0700

    Finished but needed help with #3

--- solution5.py
+++ solution5.py
@@ -1,1 +1,11 @@
 # Write code for algorithm 5 below
+def is_palindrome(str):
+  if len(str) == 1 or len(str) == 0:
+    return True
+  else:
+    return (str[0] == str[-1]) and is_palindrome(str[1:-1])
+
+print("is 'burrito' a palindrome?")
+print(is_palindrome('burrito'))
+print("is 'madam' a palindrome?")
+print(is_palindrome('madam'))
